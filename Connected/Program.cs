b3b49b4cf6e7a84fc0a87ccf2fc7ed8da30079a8
Nur Sakinah <nursakinah2402@gmail.com>
2022-06-08 16:55:17 +0700

    Manipulating data with connected environment

--- Connected/Program.cs
+++ Connected/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Connected
 {
@@ -6,7 +10,27 @@ namespace Connected
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connectionString = GetConnectionString();
+            string query1 = "select * from Pembimbing_Akademik where NIK=333";
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd1 = new SqlCommand(query1, cn); cn.Open();
+                using (SqlDataReader dr1 = cmd1.ExecuteReader())
+                {
+                    while (dr1.Read())
+                    {
+                        string query2 = "UPDATE Pembimbing_Akademik SET Keahlian ='Jaringan' WHERE NIK = 333";
+                        SqlCommand cmd2 = new SqlCommand(query2, cn);
+                        cmd2.ExecuteNonQuery();
+                        Console.WriteLine("Data has been updated");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+        private static string GetConnectionString()
+        {
+            return "data source=LAPTOP-KINALAKA\\NURSAKINAH;database=ProdiTI;User Id=sa;Password=Palu24072002";
         }
     }
 }
