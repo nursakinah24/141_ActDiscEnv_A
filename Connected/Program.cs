5893bba77925e3e36779bfb343e40c9114da2beb
Nur Sakinah <nursakinah2402@gmail.com>
2022-06-08 16:06:20 +0700

    Add project files.

---
+++ Connected/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Connected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
